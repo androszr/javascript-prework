afe232bb11a622ff972fea6121eb1388ed9e0573
androszr <androszr@gmail.com>
2020-04-29 14:35:09 +0200

    Add player move and configure game result

--- js/functions.js
+++ js/functions.js
@@ -16,8 +16,73 @@ function getRandomNumber() {
         computerMove = 'kamień';
     } else if (randomNumber==2) {
         computerMove = 'papier';
-    } else {
+    } else if (randomNumber==3) {
         computerMove = 'nożyce';
+    } else {
+        computerMove = 'error';
     }
     return(computerMove);
+}
+
+function getPlayerNumber() {
+    let msg = '';
+    let playerInput = '';
+    while (playerInput !== '1' && playerInput !== '2' && playerInput !== '3') {
+        if (playerInput=='') {
+            msg ='Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.';
+        } else {
+            alert('Błędnie wpisany ruch! Wpisano: ' + playerInput + '.');
+            msg='Spróbuj ponownie 1: kamień, 2: papier, 3: nożyce.'
+        };
+       playerInput = prompt(msg);
+    }
+    if (playerInput == '1') {
+        playerInput = 'kamień';
+    } else if (playerInput == '2') {
+        playerInput = 'papier';
+    } else if (playerInput == '3') {
+        playerInput = 'nożyce';
+    } else {   
+        playerInput = 'error';
+    }
+    return(playerInput);
+}
+
+function getResult() {
+    let result = '';
+    if (playerInput == 'kamień') {
+        if (computerMove == 'kamień') {
+            result = 'remis';
+        } else if (computerMove == 'papier') {
+            result = 'Komputer';
+        } else if (computerMove == 'nożyce') {
+            result = 'Gracz';
+        } else {
+            result = 'error';
+        }
+    } else if (playerInput == 'papier') {
+        if (computerMove == 'kamień') {
+            result = 'Gracz';
+        } else if (computerMove == 'papier') {
+            result = 'remis';
+        } else if (computerMove == 'nożyce') {
+            result = 'Komputer';
+        } else {
+            result = 'error';
+        }
+
+    } else if (playerInput == 'nożyce') {
+        if (computerMove == 'kamień') {
+            result = 'Komputer';
+        } else if (computerMove == 'papier') {
+            result = 'Gracz';
+        } else if (computerMove == 'nożyce') {
+            result = 'remis';
+        } else {
+            result = 'error';
+        }
+    } else {
+        result = 'error';
+    }
+    return(result);
 }
--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
 let computerMove = getRandomNumber();
+let playerInput = getPlayerNumber();
+let result = getResult();
 
-printMessage('<p>Komputer zagrał <strong>' + computerMove + '</strong>'); 
+printMessage('<p>Ty zagrałeś <strong>' + playerInput + '</strong></p>'); 
+printMessage('<p>Komputer zagrał <strong>' + computerMove + '</strong></p>'); 
+if (result == 'remis') {
+    printMessage('Mamy remis! Zagraj jeszcze raz.');
+} else if (result == 'error') {
+    printMessage('Błąd gry! Spróbuj ponownie.');
+} else {
+    printMessage('<h1>Wygrywa <strong>' + result + '!</strong></h1>');
+}
+ 
