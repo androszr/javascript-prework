5d0159997532f25e338730c9b396af139b19fb70
androszr <androszr@gmail.com>
2020-05-03 10:26:20 +0200

    Add error catch and switch default statements in game

--- js/functions.js
+++ js/functions.js
@@ -1,6 +1,6 @@
 let playerScore = 0;
 let computerScore = 0;
-let gameState=0;
+let gameState = 0;
 
 const translate = {
     stone: 'kamień',
@@ -36,6 +36,9 @@ function cheatMode(computer) {
         case Object.keys(translate)[2]:
             document.getElementById('header-photo').style.backgroundImage = "url('../images/background-stone.png')";
             break;
+        default:
+            console.log('Wrong variable content for computer:' + computer);
+            break;
     }
 }
 
@@ -51,26 +54,35 @@ function clearMessages(){
 
 
 function playGame(player){
-    let result = getResult(player, computer);
-    player = translateValue(player);
-    computer = translateValue(computer);
-    document.getElementById('buttons').style.display = 'none';
-    printMessage('<p>Ty zagrałeś <strong>' + player + '</strong></p> <p>Komputer zagrał <strong>' + computer + '</strong></p>'); 
-    switch(result) {
-        case 0:
-            printMessage('<p>Mamy remis! Zagraj jeszcze raz.</p>');
-            break;
-        case 1:
-            playerScore++;
-            document.getElementById('result-player').innerHTML = playerScore;
-            printMessage('<h1><strong>Wygrywasz!</strong></h1>');
-            break;
-        case 2:
-            computerScore++;
-            document.getElementById('result-computer').innerHTML = computerScore;
-            printMessage('<p>Wygrywa komputer. Próbuj dalej!</p>');
-            break;
+
+    try {
+        let result = getResult(player, computer);
+        player = translateValue(player);
+        computer = translateValue(computer);
+        document.getElementById('buttons').style.display = 'none';
+        printMessage('<p>Ty zagrałeś <strong>' + player + '</strong></p> <p>Komputer zagrał <strong>' + computer + '</strong></p>');  
+        switch(result) {
+            case 0:
+                printMessage('<p>Mamy remis! Zagraj jeszcze raz.</p>');
+                break;
+            case 1:
+                playerScore++;
+                document.getElementById('result-player').innerHTML = playerScore;
+                printMessage('<h1><strong>Wygrywasz!</strong></h1>');
+                break;
+            case 2:
+                computerScore++;
+                document.getElementById('result-computer').innerHTML = computerScore;
+                printMessage('<p>Wygrywa komputer. Próbuj dalej!</p>');
+                break;
+            default:
+                console.log('Unable to retriev result code:' + result);
+                break;
+        }   
     }
+    catch(error) {
+        console.log(error);
+    } 
 };
 
 
@@ -94,49 +106,55 @@ function getRandomMove() {
             return Object.keys(translate)[1];
         case 3:
             return Object.keys(translate)[2];
+        default:
+            console.log('Unable to generate random number code:' + randomNumber);
+            break;
     }
     
 }
 
 function getResult(player, computer) {
-    document.getElementById('restart').style.display = 'block';
-
-    switch(player) {
-        case Object.keys(translate)[0]:
-            switch(computer) {
-                case Object.keys(translate)[0]:
-                    return 0;
-                case Object.keys(translate)[1]:
-                    return 2;
-                case Object.keys(translate)[2]:
-                    return 1;
-            }
-
-        case Object.keys(translate)[1]:
-            switch(computer) {
-                case Object.keys(translate)[0]:
-                    return 1;
-
-                case Object.keys(translate)[1]:
-                    return 0;
-
-                case Object.keys(translate)[2]:
-                    return 2
-            }
-
-        case Object.keys(translate)[2]:
-            switch(computer) {
-                case Object.keys(translate)[0]:
-                    return 2;
-
-                case Object.keys(translate)[1]:
-                    return 1;
-
-                case Object.keys(translate)[2]:
-                    return 0;
-            }
+    
+    try {
+        document.getElementById('restart').style.display = 'block';  
+        switch(player) {
+            case Object.keys(translate)[0]:
+                switch(computer) {
+                    case Object.keys(translate)[0]:
+                        return 0;
+                    case Object.keys(translate)[1]:
+                        return 2;
+                    case Object.keys(translate)[2]:
+                        return 1;
+                }
+            case Object.keys(translate)[1]:
+                switch(computer) {
+                    case Object.keys(translate)[0]:
+                        return 1;
+    
+                    case Object.keys(translate)[1]:
+                        return 0;
+    
+                    case Object.keys(translate)[2]:
+                        return 2
+                }
+    
+            case Object.keys(translate)[2]:
+                switch(computer) {
+                    case Object.keys(translate)[0]:
+                        return 2;
+    
+                    case Object.keys(translate)[1]:
+                        return 1;
+    
+                    case Object.keys(translate)[2]:
+                        return 0;
+                }
+        }
     }
-
+    catch(error) {
+        console.log(error);
+    }   
 }
 
 function gameRestart() {
@@ -152,25 +170,30 @@ function gameRestart() {
 
 document.addEventListener('keydown', logKey);
 function logKey(e) {
-    if (gameState==1) {
-        switch(e.which) {
-            case 82:
-                gameRestart();
-        }
-    } else {
-        switch(e.which) {
-            case 49:
-                playGame(Object.keys(translate)[0]);
-                gameState=1;
-                break;
-            case 50:
-                playGame(Object.keys(translate)[1]);
-                gameState=1;
-                break;
-            case 51:
-                playGame(Object.keys(translate)[2]);
-                gameState=1;
-                break;
-        }
-    }     
+    try {
+        if (gameState == 1) {
+            switch(e.which) {
+                case 82:
+                    gameRestart();
+            }
+        } else {
+            switch(e.which) {
+                case 49:
+                    playGame(Object.keys(translate)[0]);
+                    gameState=1;
+                    break;
+                case 50:
+                    playGame(Object.keys(translate)[1]);
+                    gameState=1;
+                    break;
+                case 51:
+                    playGame(Object.keys(translate)[2]);
+                    gameState=1;
+                    break;
+            }
+        }        
+    }
+    catch(error) {
+        console.log(error);
+    } 
 }
